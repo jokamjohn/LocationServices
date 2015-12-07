819dacef1041da62627ad5a39291333aec66470b
johnkagga <johnkagga@gmail.com>
2015-12-07 23:56:13 +0300

    Refactoring

--- app/src/main/java/johnkagga/me/location/MainActivity.java
+++ app/src/main/java/johnkagga/me/location/MainActivity.java
@@ -13,6 +13,9 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
                 GoogleApiClient.OnConnectionFailedListener, LocationListener{
 
@@ -20,6 +23,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private TextView mTextView;
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
+    private TextView mTimeTextview;
+    private TextView mLongTextview;
+    private String mLastUpdatedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +40,17 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 .addOnConnectionFailedListener(this)
                 .build();
 
+        initializeScreen();
+
+    }
+
+    /**
+     * Initializing the Screen views
+     */
+    protected void initializeScreen() {
         mTextView = (TextView) findViewById(R.id.textViewLocation);
+        mTimeTextview = (TextView) findViewById(R.id.textviewTime);
+        mLongTextview = (TextView) findViewById(R.id.textViewlongitude);
     }
 
     @Override
@@ -57,6 +73,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     }
 
+    /**
+     * Getting Location Updates from the OnConnected
+     */
     protected void startLocationUpdates() {
         mLocationRequest = LocationRequest.create();
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
@@ -80,11 +99,18 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onLocationChanged(Location location) {
         Log.i(LOG_TAG,location.toString());
+        mLastUpdatedTime = DateFormat.getTimeInstance().format(new Date());
 
         updateUi(location);
     }
 
+    /**
+     * Upadting the User Interface
+     * @param location Location object
+     */
     protected void updateUi(Location location) {
-        mTextView.setText(location.toString());
+        mTextView.setText(String.valueOf(location.getLatitude()));
+        mLongTextview.setText(String.valueOf(location.getLongitude()));
+        mTimeTextview.setText(mLastUpdatedTime);
     }
 }
