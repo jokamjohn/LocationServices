11aa6cb8bdceb4501c13e94ee87cb3b1a264fcbb
johnkagga <johnkagga@gmail.com>
2015-12-08 20:21:17 +0300

    Notification implementation

--- app/src/main/java/johnkagga/me/location/services/GeoFenceTransitionIntentService.java
+++ app/src/main/java/johnkagga/me/location/services/GeoFenceTransitionIntentService.java
@@ -1,7 +1,14 @@
 package johnkagga.me.location.services;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.android.gms.location.Geofence;
@@ -9,6 +16,8 @@ import com.google.android.gms.location.GeofencingEvent;
 
 import java.util.List;
 
+import johnkagga.me.location.MainActivity;
+import johnkagga.me.location.R;
 import johnkagga.me.location.helper;
 
 /**
@@ -52,7 +61,56 @@ public class GeoFenceTransitionIntentService extends IntentService {
             //GeoFence transition string
             String geoFenceTransitionDetails = helper.getTransitionDetails(this,
                     transitionType,triggeringGeoFences);
+
+            sendNotification(geoFenceTransitionDetails);
         }
 
     }
+
+    /**
+     * Sending notification
+     * @param notificationDetails Notification message
+     */
+    private void sendNotification(String notificationDetails) {
+        // Create an explicit content Intent that starts the main Activity.
+        Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
+
+        // Construct a task stack.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+
+        // Add the main Activity to the task stack as the parent.
+        stackBuilder.addParentStack(MainActivity.class);
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent);
+
+        // Get a PendingIntent containing the entire back stack.
+        PendingIntent notificationPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Get a notification builder that's compatible with platform versions >= 4
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+        // Define the notification settings.
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+                // In a real app, you may want to use a library like Volley
+                // to decode the Bitmap.
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),
+                        R.mipmap.ic_launcher))
+                .setColor(Color.RED)
+                .setContentTitle(notificationDetails)
+                .setContentText(getString(R.string.geofence_transition_notification_text))
+                .setContentIntent(notificationPendingIntent);
+
+        // Dismiss notification once the user touches it.
+        builder.setAutoCancel(true);
+
+        // Get an instance of the Notification manager
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Issue the notification
+        mNotificationManager.notify(0, builder.build());
+    }
+
 }
