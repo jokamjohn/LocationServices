020607a44a5406f3bca8b1384dd645cedd2a6e48
johnkagga <johnkagga@gmail.com>
2015-12-08 20:58:43 +0300

    Added LatLong for the chosen locations

--- app/src/main/java/johnkagga/me/location/Constants.java
+++ app/src/main/java/johnkagga/me/location/Constants.java
@@ -1,9 +1,13 @@
 package johnkagga.me.location;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.HashMap;
+
 /**
  * Created by jokamjohn on 12/8/2015.
  */
-public class Constants {
+public final class Constants {
 
     public static final String PACKAGE_NAME = "johnkagga.me.location";
     public static final String BROADCAST_ACTION = PACKAGE_NAME + ".BROADCAST_ACTION";
@@ -12,6 +16,26 @@ public class Constants {
 
     public static final long BROADCAST_INTERVAL = 1000;
 
+    public static final float GEOFENCE_RADIUS_IN_METERS = 1609; // 1 mile
+    public static final long GEOFENCE_EXPIRATION_IN_HOURS = 12;
+    public static final long GEOFENCE_EXPIRATION_IN_MILLISECONDS =
+            GEOFENCE_EXPIRATION_IN_HOURS * 60 * 60 * 1000;
 
+    public static final HashMap<String, LatLng> HOSTEL_GEO_CODES =
+            new HashMap<>();
+    static {
+        //Panicol hostel
+        HOSTEL_GEO_CODES.put("Panicol",new LatLng(0.3393549,32.5622852));
+
+        //Nana Hostel
+        HOSTEL_GEO_CODES.put("Nana", new LatLng(0.3249589,32.5642499));
+
+        //Tech Mak
+        HOSTEL_GEO_CODES.put("Tech", new LatLng(0.337239,32.5621557));
+
+        //Olympia hostel
+        HOSTEL_GEO_CODES.put("Olympia", new LatLng(0.3358354,32.5583293));
+
+    }
 
 }
--- app/src/main/java/johnkagga/me/location/GeoFencingActivity.java
+++ app/src/main/java/johnkagga/me/location/GeoFencingActivity.java
@@ -5,9 +5,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
-public class GeoFencingActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
+
+public class GeoFencingActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener{
+
+    private static final String LOG_TAG = GeoFencingActivity.class.getSimpleName();
+    private GoogleApiClient mGoogleApiClient;
+    private LocationServices mLocationServices;
+    private ArrayList<Geofence> mGeofenceArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +38,40 @@ public class GeoFencingActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void populateGeoFenceList()
+    {
+
+    }
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(LOG_TAG,"Connection failed");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        if (mGoogleApiClient.isConnected())
+        {
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
     }
 
 }
